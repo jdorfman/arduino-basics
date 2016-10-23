4de81e5e95a90ad911506b848adaf96b0f6548af
Justin Dorfman <justindorfman@gmail.com>
2016-10-23 13:16:52 -0700

    Added board cleanup example

---
+++ j5-examples/board-cleanup.js
@@ -0,0 +1,11 @@
+var five = require('johnny-five')
+var board = new five.Board()
+
+board.on('ready', function () {
+  var led = new five.Led(13)
+  led.on()
+
+  this.on('exit', function () {
+    led.off()
+  })
+})
