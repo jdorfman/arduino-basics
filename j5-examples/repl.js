ff2c940dc837b88918de3f17e89c5e164cf6d37f
Justin Dorfman <justindorfman@gmail.com>
2016-10-23 19:25:31 -0700

    Added modified REPL example

---
+++ j5-examples/repl.js
@@ -0,0 +1,27 @@
+// via http://johnny-five.io/examples/repl/
+
+var five = require('johnny-five')
+var board = new five.Board()
+
+board.on('ready', function () {
+  console.log('Welcome to the Arduino Basics REPL. Hit enter to begin...')
+
+  var led = new five.Led(13)
+
+  this.repl.inject({
+    // Allow limited on/off control access to the
+    // Led instance from the REPL.
+    // >> on()
+    on: function () {
+      led.on()
+    },
+    // >> strobe()
+    strobe: function () {
+      led.strobe(50) // Bug: Doesn't turn off
+    },
+    // >> off()
+    off: function () {
+      led.off()
+    }
+  })
+})
