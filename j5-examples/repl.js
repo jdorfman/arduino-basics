3c980bcd5557fb59883b1658ec108766eef1082d
Justin Dorfman <justindorfman@gmail.com>
2016-10-24 19:21:38 -0700

    Added LED 12 & 11 to repl.js

--- j5-examples/repl.js
+++ j5-examples/repl.js
@@ -6,26 +6,36 @@ var board = new five.Board()
 board.on('ready', function () {
   console.log('Welcome to the Arduino Basics REPL. Hit enter to begin...')
 
-  var led = new five.Led(13)
+  var led13 = new five.Led(13)
+  var led12 = new five.Led(12)
+  var led11 = new five.Led(11)
 
   this.repl.inject({
     // Allow limited on/off control access to the
     // Led instance from the REPL.
     // >> on()
     on: function () {
-      led.on()
+      led13.on()
+      led12.on()
+      led11.on()
     },
     // >> strobe()
     strobe: function () {
-      led.strobe(50)
+      led13.strobe(100)
+      led12.strobe(75)
+      led11.strobe(50)
     },
     // >> stop()
     stop: function () {
-      led.stop()
+      led13.stop()
+      led12.stop()
+      led11.stop()
     },
     // >> off()
     off: function () {
-      led.off()
+      led13.off()
+      led12.off()
+      led11.off()
     }
   })
 })
