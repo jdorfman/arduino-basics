52ce236e4efa8e1e770b2f996e4bce8b760aa126
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-28 21:57:07 -0700

    reverse the camelCasing

--- j5-examples/annoying.js
+++ j5-examples/annoying.js
@@ -1,19 +1,19 @@
 // via http://node-ardx.org/exercises/6
 
-var five = require('johnny-five')
-five.Board().on('ready', function () {
-  var piezo = new five.Piezo(13)
-  var led7 = new five.Led(7)
-  var led13 = new five.Led(10)
-  var led3 = new five.Led(3)
-  var led12 = new five.Led(8)
-  var led5 = new five.Led(5)
+var fIVE = require('johnny-five')
+fIVE.Board().on('ready', function () {
+  var piezo = new fIVE.Piezo(13)
+  var LEDyELLOW = new fIVE.Led(3)
+  var LEDwHITE = new fIVE.Led(5)
+  var LEDbLUE = new fIVE.Led(7)
+  var LEDgREEN = new fIVE.Led(9)
+  var LEDrED = new fIVE.Led(11)
   var val = 0
-  led13.strobe(230)
-  led12.strobe(440)
-  led3.strobe(500)
-  led5.strobe(80)
-  led7.strobe(61)
+  LEDyELLOW.strobe(500)
+  LEDwHITE.strobe(80)
+  LEDbLUE.strobe(61)
+  LEDgREEN.strobe(440)
+  LEDrED.strobe(230)
   this.loop(300, function () {
     if (val ^= 1) {
       piezo.play({
